4df7b2fdfb3739276d423a66638cb1199c37a01f
Laurent Signorel <nerzool@hotmail.com>
2019-11-14 18:55:54 +0100

    1st commit

---
+++ Mouseclicker/MouseOperations.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Mouseclicker
+{
+    public class MouseOperations
+    {
+        [Flags]
+        public enum MouseEventFlags
+        {
+            LeftDown = 0x00000002,
+            LeftUp = 0x00000004,
+            MiddleDown = 0x00000020,
+            MiddleUp = 0x00000040,
+            Move = 0x00000001,
+            Absolute = 0x00008000,
+            RightDown = 0x00000008,
+            RightUp = 0x00000010
+        }
+
+        [DllImport("user32.dll", EntryPoint = "SetCursorPos")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetCursorPos(int x, int y);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetCursorPos(out MousePoint lpMousePoint);
+
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        public static void SetCursorPosition(int x, int y)
+        {
+            SetCursorPos(x, y);
+        }
+
+        public static void SetCursorPosition(MousePoint point)
+        {
+            SetCursorPos(point.X, point.Y);
+        }
+
+        public static MousePoint GetCursorPosition()
+        {
+            MousePoint currentMousePoint;
+            var gotPoint = GetCursorPos(out currentMousePoint);
+            if (!gotPoint) { currentMousePoint = new MousePoint(0, 0); }
+            return currentMousePoint;
+        }
+
+        public static void MouseEvent(MouseEventFlags value)
+        {
+            MousePoint position = GetCursorPosition();
+
+            mouse_event
+                ((int)value,
+                 position.X,
+                 position.Y,
+                 0,
+                 0)
+                ;
+        }
+
+        public static void DoMouseClick()
+        {
+            var pos = GetCursorPosition();
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, pos.X, pos.Y, 0, 0);
+        }
+
+        public static void DoMouseClick(int x, int y)
+        {
+            SetCursorPos(x, y);
+            Thread.Sleep(20);
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+            Thread.Sleep(20);
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MousePoint
+        {
+            public int X;
+            public int Y;
+
+            public MousePoint(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+    }
+}
---
+++ Mouseclicker/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mouseclicker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CalculAllerRetoursDeBricabracVersWham();
+
+            //RobotclickMaison();
+
+
+            Console.ReadKey();
+        }
+
+        private static void RobotclickMaison()
+        {
+            //affichage de la position de la souris
+            //var pos = MouseOperations.GetCursorPosition();
+            //Console.WriteLine($"X: {pos.X} - Y: {pos.Y}");
+            //Console.ReadKey();
+
+            //synchro off
+            Console.ReadKey();
+            MouseOperations.DoMouseClick(145, 15);
+            MouseOperations.DoMouseClick(1375, 475);
+            MouseOperations.DoMouseClick(345, 15);
+            MouseOperations.DoMouseClick(1375, 475);
+            MouseOperations.DoMouseClick(545, 15);
+            MouseOperations.DoMouseClick(1375, 475);
+            MouseOperations.DoMouseClick(745, 15);
+            MouseOperations.DoMouseClick(1375, 475);
+        }
+
+        private static void CalculAllerRetoursDeBricabracVersWham()
+        {
+            DateTime premiereAttaque = new DateTime(2019, 11, 14, 17, 49, 41);
+
+            TimeSpan aller = new TimeSpan(01, 22, 28);
+
+            var retour = premiereAttaque + aller;
+
+            TimeSpan allerRetour = new TimeSpan(2, 44, 56);
+
+            var deuxiemeAttaque = premiereAttaque + allerRetour;
+
+            Console.WriteLine(retour);
+            Console.WriteLine(deuxiemeAttaque.ToString());
+        }
+    }
+}
